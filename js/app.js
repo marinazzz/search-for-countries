3b6170992f0c645326c292b6006d11efb98937d4
Marina Zujic <zujicmarina@gmail.com>
2020-08-26 22:03:55 +0200

    Included helper class for grid, shorter styles for cards

---
+++ js/app.js
@@ -0,0 +1,12 @@
+// let select = new SlimSelect({
+//     select: '#slim-select',
+//     showSearch: false,
+//     placeholder: 'Filter by Region',
+
+//     data: [
+//         { text: 'Value 1' },
+//         { text: 'Value 2' },
+//         { text: 'Value 3' }
+//     ]
+// })
+
