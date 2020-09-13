2ed60cc8813c7e7da3e43aec75a9de6e30d4b089
Marina Zujic <zujicmarina@gmail.com>
2020-09-13 15:16:05 +0200

    Changed ul to dl list on details page

--- js/details.js
+++ js/details.js
@@ -12,15 +12,9 @@ if (countryID) {
     }
   });
   // implement error handling
-} else {
-  console.log("Something wrong");
 }
 
-/* TODO: implement country detais fetching using the
-   https://restcountries.eu/#api-endpoints-name end point
-*/
-
-function formQueryParamFromAray(countryCodeArr) {
+function formQueryParamFromArray(countryCodeArr) {
   let queryStringArr = "";
   countryCodeArr.forEach((countryCode) => {
     queryStringArr += countryCode + ";";
@@ -32,10 +26,10 @@ async function getCountryDetails(id) {
   const response = await fetch(`https://restcountries.eu/rest/v2/alpha/${id}`);
   return response.json();
 }
-// recieves an array of country iso 3 letter codes as an argment
+// recieves an array of country iso 3 letter codes as an argument
 async function getNeighbouringCountries(countryIds) {
   const response = await fetch(
-    `https://restcountries.eu/rest/v2/alpha?codes=${formQueryParamFromAray(
+    `https://restcountries.eu/rest/v2/alpha?codes=${formQueryParamFromArray(
       countryIds
     )}`
   );
@@ -48,62 +42,69 @@ function renderCountryDetails(countryData, neighbouringCountries) {
   if (neighbouringCountries !== null) {
     neighouringCountriesList = `
       <div class="pad-md-top">
-        <div class="card__info-details card__info-details--no-pad">Border Countries:
-          <div class="links-row">`;
+        <div class="card__info-details">
+         <h3>Border Countries:</h3>
+         <div class="links-row">`;
     neighbouringCountries.forEach((country) => {
       neighouringCountriesList += `
       <span class="main-btn">${country.name}</span>
       `;
     });
     neighouringCountriesList += `
+          </div>
         </div>
-      </div>
-    </div>`;
+      </div>`;
   }
-  // replace ul with definition lists
   grid += `
-    <div class="card__image pad-md-bot">
-    <img src="${countryData.flag}" alt="${countryData.name} flag" />
+    <div class="pad-md-bot">
+     <img class="card__flag" src="${countryData.flag}" alt="${countryData.name} flag" />
     </div>
     <div>
       <h2 class="pad-md-bot">${countryData.name}</h2>
       <div class="grid grid--xl-gap pad-md-bot pad-md-top">
-        <ul class="pad-md-bot">
-          <li class="card__info-details">Native Name:
-            <span>${countryData.nativeName}</span>
-          </li>
-          <li class="card__info-details">Population:
-            <span>${countryData.population}</span>
-          </li>
-          <li class="card__info-details">Region:
-            <span>${countryData.region}</span>
-          </li>
-          <li class="card__info-details">Sub Region:
-            <span>${countryData.subregion}</span>
-          </li>
-          <li class="card__info-details">Capital:
-            <span>${countryData.capital}</span>
-          </li>
-        </ul>
-        <ul>
-          <li class="card__info-details">Top Level Domain:
-            <span>${countryData.topLevelDomain}</span>
-          </li>
-          <li class="card__info-details">Currencies:
-            <span>${countryData.currencies.map(
-              (currency) => currency.name
-            )}</span>
-          </li>
-          <li class="card__info-details">Language:
-            <span>${countryData.languages.map(
-              (language) => language.name
-            )}</span>
-          </li>
-        </ul>
+        <dl class="pad-md-bot">
+          <div class="card__info-details">
+             <dt>Native Name:</dt>
+             <dd>${countryData.nativeName}</dd>
+          </div>
+          <div class="card__info-details">
+             <dt>Population:</dt>
+             <dd>${countryData.population}</dd>
+          </div>
+          <div class="card__info-details">
+             <dt>Region:</dt>
+             <dd>${countryData.region}</dd>
+          </div>
+          <div class="card__info-details">
+             <dt>Sub Region:</dt>
+             <dd>${countryData.subregion}</dd>
+          </div>
+          <div class="card__info-details">
+             <dt>Capital:</dt>
+             <dd>${countryData.capital}</dd>
+          </div>
+        </dl>
+        <dl>
+          <div class="card__info-details">
+             <dt>Top Level Domain:</dt>
+             <dd>${countryData.topLevelDomain}</dd>
+           </div>
+          <div class="card__info-details">
+             <dt>Currencies:</dt>
+             <dd>${countryData.currencies.map(
+                 (currency) => currency.name
+               )}</dd>
+          </div>
+          <div class="card__info-details">
+              <dt>Language:</dt>
+              <dd>${countryData.languages.map(
+               (language) => language.name
+              )}</dd>
+           </div>
+        </dl>
       </div>
       ${neighouringCountriesList}
     </div>
   `;
-  console.log(neighouringCountriesList);
   document.getElementById("countryDetails").innerHTML = grid;
 }
